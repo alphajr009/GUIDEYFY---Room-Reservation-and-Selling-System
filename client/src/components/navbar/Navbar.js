b9909dcfc4336bfaff3a35ad38a15f70e1de3971
Pasindu Randika <pasindurandika53@gmail.com>
2023-04-08 12:07:15 +0530

    navbar edit
    navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import logo from '../../images/logo.png';
+
 
 function Navbar() {
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className="navbar">
+      <img className="navbar-logo" src={logo} alt="Logo" />
+      <div className="navbar-text">
+        <h1>Let’s start the Guide</h1>
+        <p>Search best accommodation and much more.</p>
+      </div>
+      <div className="navbar-buttons">
+        <button>List your property</button>
+        <button>Register</button>
+        <button>Sign in</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

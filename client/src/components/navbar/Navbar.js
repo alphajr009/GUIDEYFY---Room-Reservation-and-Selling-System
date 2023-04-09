5906daa06dab6ea1d33d9df1d54d74f6d7d5c8bf
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-04-09 13:16:46 +0530

    serachbar create

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../images/logo.png';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faLocationArrow, faPerson, faSearch } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
   return (
     <div className="navbar">
@@ -12,7 +13,33 @@ function Navbar() {
             <h1>Let’s start the Guide</h1>
             <p>Search best accommodation and much more.</p>
           </div>
+          < div className='search'>
+              <div className='searchItem'>
+                <FontAwesomeIcon icon={faLocationArrow} className='searchIcon' />
+                <input
+                  type="text"
+                  placeholder="Where's your destination ?"
+                  className="searchInput" />
+              </div>
+      
+            <div className='searchItem'>
+                <FontAwesomeIcon icon={faPerson} className='searchIcon' />
+                <span className='searchText'>0 childern 1 adult 0 room</span>
+            </div>
+
+            <div className='searchItem'>
+                <FontAwesomeIcon icon={faCalendar} className='searchIcon' />
+                <span className='searchText'>date to date</span>
+            </div>
+
+            <div className='searchItem'>
+                <FontAwesomeIcon icon={faSearch} className='searchIcon' />
+              </div>
+           
+          </div>
+
         </div>
+
         <div className="navbar-buttons">
           <button className='btn-list'>List your property</button>
           <button className='btn-reg'>Register</button>
@@ -20,6 +47,7 @@ function Navbar() {
         </div>
       </div>
     </div>
+
   );
 }
 

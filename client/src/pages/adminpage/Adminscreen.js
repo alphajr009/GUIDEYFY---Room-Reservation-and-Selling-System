6fa60b11152d7b655baeaf06a145255da98135ec
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-04-12 04:37:56 +0530

    All Footers Completed
    user, admin, seller footer done and used the containers  correctly :) :+1:

--- client/src/pages/adminpage/Adminscreen.js
+++ client/src/pages/adminpage/Adminscreen.js
@@ -1,4 +1,4 @@
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 import './adminscreen.css'
 import AdminNavbar from '../../components/adminNavbar/AdminNavbar'
 import { Layout } from "antd";
@@ -8,34 +8,34 @@ import Finance from './Finance'
 import Users from './Users'
 import Sellers from './Sellers'
 import Blogs from './Blogs'
+import Footeradmin from '../../components/adminfooter/Footeradmin';
 
 
 
 
 function Adminscreen() {
 
-    const [activeTab, setActiveTab] = useState("booking");
+  const [activeTab, setActiveTab] = useState("booking");
 
 
   return (
-    <div className='admincontainer'>
-<AdminNavbar setActiveTab={setActiveTab} activeTab={activeTab} />
+    <div className='adminscreen'>
+      <div className='admincontainer'>
+        <AdminNavbar setActiveTab={setActiveTab} activeTab={activeTab} />
         <div className="admin">
-
-      <div className="admin__main">
-        <Layout.Content>
-          {activeTab === "booking" && <Bookings/>}
-          {activeTab === "rooms" && <Rooms />}
-          {activeTab === "finance" && <Finance />}
-          {activeTab === "users" && <Users />}
-          {activeTab === "sellers" && <Sellers />}
-          {activeTab === "blogs" && <Blogs />}
-          
-
-        </Layout.Content>
+          <div className="admin__main">
+            <Layout.Content>
+              {activeTab === "booking" && <Bookings />}
+              {activeTab === "rooms" && <Rooms />}
+              {activeTab === "finance" && <Finance />}
+              {activeTab === "users" && <Users />}
+              {activeTab === "sellers" && <Sellers />}
+              {activeTab === "blogs" && <Blogs />}
+            </Layout.Content>
+          </div>
+        </div>  
       </div>
-    </div>
-
+      <Footeradmin/>
     </div>
   )
 }
--- client/src/pages/sellerpage/Sellerscreen.js
+++ client/src/pages/sellerpage/Sellerscreen.js
@@ -1,4 +1,5 @@
 import { React, useState } from 'react'
+import './sellerscreen.css'
 import { Layout } from "antd";
 import Sellernavbar from '../../components/sellernavbar/Sellernavbar'
 import Blog from './Blog'
@@ -9,32 +10,32 @@ import Reservations from './Reservations'
 import Property from './Property'
 import Analytics from './Analytics'
 import Reviews from './Reviews'
+import Footerseller from '../../components/sellerFooter/Footerseller';
 
 
 function Sellerscreen() {
 
   const [activeTab, setActiveTab] = useState("home");
   return (
-    <div className='admincontainer'>
-      <Sellernavbar setActiveTab={setActiveTab} activeTab={activeTab} />
-      <div className="seller">
-
-        <div className="seller_main">
-          <Layout.Content>
-            {activeTab === "home" && <Home />}
-            {activeTab === "reservations" && <Reservations />}
-            {activeTab === "property" && <Property />}
-            {activeTab === "promotions" && <Promotion />}
-            {activeTab === "blog" && <Blog />}
-            {activeTab === "reviews" && <Reviews />}
-            {activeTab === "payments" && < Payments/>}
-            {activeTab === "analytics" && <Analytics />}
-
-
-          </Layout.Content>
+    <div className='sellerscreen'>
+      <div className='sellercontainer'>
+        <Sellernavbar setActiveTab={setActiveTab} activeTab={activeTab} />
+        <div className="seller">
+          <div className="seller_main">
+            <Layout.Content>
+              {activeTab === "home" && <Home />}
+              {activeTab === "reservations" && <Reservations />}
+              {activeTab === "property" && <Property />}
+              {activeTab === "promotions" && <Promotion />}
+              {activeTab === "blog" && <Blog />}
+              {activeTab === "reviews" && <Reviews />}
+              {activeTab === "payments" && < Payments/>}
+              {activeTab === "analytics" && <Analytics />}
+            </Layout.Content>
+          </div>
         </div>
       </div>
-
+      <Footerseller/>
     </div>
   )
 }

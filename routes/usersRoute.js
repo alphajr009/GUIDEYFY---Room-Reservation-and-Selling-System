61993b93cbe95e190de985dd6fd10110d7318590
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-04-15 12:00:47 +0530

    Complete sign in backend
    Complete sign in backend

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -27,5 +27,32 @@ router.post("/register", async (req, res) => {
 });
 
 
+router.post("/login", async (req, res) => {
+
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email: email, password: password })
+        if (user) {
+
+            const temp = {
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                isUser: user.isUser,
+                _id: user._id,
+            }
+            res.send(temp);
+        }
+        else {
+            return res.status(400).json({ message: 'Login Failed' });
+        }
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+
+});
+
+
 module.exports = router
 

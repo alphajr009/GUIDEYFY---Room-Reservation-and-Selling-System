a6eb19a2e6107a110b6b549f8ce5e27e65fc7954
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-04-21 15:34:36 +0530

    Seller Central Home Deleted
    Remove Seller Central Home

--- client/src/pages/sellerpage/homes/Home.js
+++ client/src/pages/sellerpage/homes/Home.js
@@ -10,50 +10,8 @@ document.body.appendChild(script);
 function Home() {
   return (
     <div>
-      <table className="SellerDiscriptionBarAvi">
-        <tbody>
-          <tr>
-            <th>Reservations</th>
-            <th>Sales (30 Days)</th>
-            <th>Properties</th>
-            <th>Seller Level</th>
-          </tr>
-          <tr>
-            <td>0</td>
-            <td>Rs.150000</td>
-            <td>45</td>
-            <td>Platinum</td>
-          </tr>
-        </tbody>
-      </table>
-      <div className="CreateBlogButtonContainerAvi">
-        <Link to="/blog" className="CreateBlogButtonAvi">
-          <span>Create a Blog</span>
-          <i class='fab fa-blogger-b'></i>
-        </Link>
-      </div>
-      <div>
-        <table className="GetStartedAvi">
-          <tbody>
-            <tr>
-              <th>Get Started</th>
-            </tr>
-            <tr>
-              <td><Link to="/blog">New Business Seller Guide</Link></td>
-            </tr>
-            <tr>
-              <td><Link to="/blog">Learn How To Create Promotion</Link></td>
-            </tr>
-            <tr>
-              <td><Link to="/blog">Learn How To Blogging</Link></td>
-            </tr>
-            <tr>
-              <td><Link to="/blog">Property Listing Best Practices</Link></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      Seller Screen
     </div>
   );
 }
-export default Home;
+export default Home;

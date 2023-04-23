c18a8dd3801787e2ebeb7bf5110bbab4ba5a1043
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-04-23 11:42:39 +0530

    Property Remove
    Property js remove

--- client/src/pages/sellerpage/property/Property.js
+++ client/src/pages/sellerpage/property/Property.js
@@ -7,37 +7,6 @@ function Property() {
       <div className="title">
         Create New Property
       </div>
-      <div className="form">
-        <div className="inputfield">
-          <label htmlFor="roomName">Room Name</label>
-          <input type="text" id="firstName" placeholder="Please Enter Room Name"className="input" />
-        </div>  
-        <div className="inputfield">
-          <label htmlFor="rentperday">Rent Perday</label>
-          <input type="text" id="lastName" placeholder="What is daily Rentel Amount?" className="input" />
-        </div>  
-        <div className="inputfield">
-          <label htmlFor="MaxCount">Max Count</label>
-          <input type="text" id="MaxCount"placeholder="How Many Guest Count?" className="input" />
-        </div>  
-        <div className="inputfield">
-          <label htmlFor="PhoneNumber">Phone Number</label>
-          <input type="text" id="PhoneNumber" placeholder="What is your Prefrred phone number?" className="input" />
-        </div> 
-        <div className="inputfield">
-          <label htmlFor="type">Type</label>
-          <div className="custom_select">
-            <select id="Type">
-              <option value="">Choose the Type of Room</option>
-              <option value="Deluxe">Deluxe</option>
-              <option value="Non-Deluxe">Non-Deluxe</option>
-            </select>
-          </div>
-      </div>
-        <div className="inputfield">
-          <input type="submit" value="Continue" className="btn" />
-        </div>
-      </div>
     </div>
   );
 }

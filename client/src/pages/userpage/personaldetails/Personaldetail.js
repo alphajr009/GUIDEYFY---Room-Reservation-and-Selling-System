d6ba74be0f490a842f35685fefbb36e3dd098ca8
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-04-29 19:54:01 +0530

    Country list updated
    Country list updated
    
    Co-Authored-By: Jayath Kasiwaththa <99715592+Jayathkasiwaththa24@users.noreply.github.com>
    Co-Authored-By: Avishka Munasinghe <it21262418@my.sliit.lk>
    Co-Authored-By: Ravindu Rusith <122794096+RavinduRusith18@users.noreply.github.com>
    Co-Authored-By: Bhashi99 <100124862+Bhashi99@users.noreply.github.com>
    Co-Authored-By: Kethmini Weerasooriya <99665491+kethu2001@users.noreply.github.com>
    Co-Authored-By: Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>

--- client/src/pages/userpage/personaldetails/Personaldetail.js
+++ client/src/pages/userpage/personaldetails/Personaldetail.js
@@ -35,6 +35,28 @@ function Personaldetail() {
 
 
 
+  const country = [
+    'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain',
+     'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 
+     'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo (Congo-Brazzaville)', 
+     'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czechia', 'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor (Timor-Leste)', 
+     'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany',
+      'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Holy See', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 
+      'Ireland', 'Israel', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 
+      'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 
+      'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar (formerly Burma)', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 
+      'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestine State', 'Panama', 'Papua New Guinea', 
+      'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 
+      'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 
+      'Somalia','South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Tajikistan', 'Tanzania', 'Thailand', 
+      'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States of America',
+       'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'
+    ];
+    
+    
+    
+    
+
 
   const [fname, setFname] = useState('');
   const [lname, setLname] = useState('');
@@ -296,7 +318,7 @@ const showEmailModal = () => {
                         message: "Please Enter a First Name"
                       }
                     ]}
-                    name="First Name"
+                    name="Fname"
                     label="First Name"
                   >
                     <Input placeholder={user.fname} />
@@ -309,7 +331,7 @@ const showEmailModal = () => {
                         message: "Please Enter a Last Name"
                       }
                     ]}
-                    name="Last Name"
+                    name="Lname"
                     label="Last Name"
                   >
                     <Input placeholder={user.lname} />

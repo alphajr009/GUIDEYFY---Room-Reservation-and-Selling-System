2785bf647134ebc5610d304a08da6bfe7a900599
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-01 21:28:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const path = require("path");
 
-
-
 const app = express();
-
 
 const dbconfig = require('./db')
 const roomsRoute = require('./routes/roomRoute')
@@ -16,14 +13,12 @@ const paymentRoute = require('./routes/paymentRoute')
 const promotionRoute = require('./routes/promotionRoute')
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://guideyfy.herokuapp.com'],
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-
 
 app.use(express.json())
 
@@ -34,20 +29,17 @@ app.use('/api/blogs', blogRoute)
 app.use('/api/payments', paymentRoute)
 app.use('/api/promotions', promotionRoute)
 
-
-
-
 const port = process.env.PORT || 5000;
 
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set the static folder
   app.use(express.static("client/build"));
 
   // Serve the index.html file for all non-API routes
-  app.get(/^\/(?!api).*/, (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 
 app.listen(port, () => console.log('Node Server Started using Nodemon!'));
